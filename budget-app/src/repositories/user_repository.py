1a93a25f58234a85ef5fb07888c2d87c49da9c91
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-11 20:46:44 +0200

    Modified files

--- budget-app/src/repositories/user_repository.py
+++ budget-app/src/repositories/user_repository.py
@@ -1,13 +1,2 @@
 from entities.user import User
 from database_connection import get_database_connection
-
-#A start of a user_repository for now
-def get_user_by_row(row):
-    return User(row["username"], row["password"]) if row else None
-
-
-class UserRepository:
-    def __init__(self, connection):
-        self._connection = connection
-
-user_repository = UserRepository(get_database_connection())
