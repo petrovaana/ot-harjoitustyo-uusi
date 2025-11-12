f3a209d20b26a0e3c8d3b4cbf1fc87b612ee4250
petrovaana <143181789+petrovaana@users.noreply.github.com>
2025-11-12 12:59:46 +0200

    Remove unused imports and BudgetRepository class
    Removed unused imports and the BudgetRepository class.

--- budget-app/src/repositories/budget_repository.py
+++ budget-app/src/repositories/budget_repository.py
@@ -1,7 +1,1 @@
-from entities.budget import Budget
-from pathlib import Path
-from repositories.user_repository import user_repository
 
-class BudgetRepository:
-    def __init__(self, file_path):
-        self._file_path = file_path
