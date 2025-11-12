5293f1277e533057c804185a6e7634d33cc87bf7
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-12 12:31:54 +0200

    Created the users database and modified database_connection.py

--- budget-app/src/database_connection.py
+++ budget-app/src/database_connection.py
@@ -1,9 +1,6 @@
-import os
 import sqlite3
 from config import DATABASE_FILE_PATH
 
-
-dirname = os.path.dirname(__file__)
 
 connection = sqlite3.connect(DATABASE_FILE_PATH)
 connection.row_factory = sqlite3.Row
