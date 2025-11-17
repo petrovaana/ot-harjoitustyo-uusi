9af08a542489e85fb68ca216875f5b6c0f29dadf
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-17 13:30:32 +0200

    Modified some parts of the repository files

--- budget-app/src/repositories/spendings_repository.py
+++ budget-app/src/repositories/spendings_repository.py
@@ -2,6 +2,9 @@ from entities.user import User
 from entities.spendings import Spendings
 from database_connection import get_database_connection
 
+#Tää on non-sense tällä hetkellä koitin vaan ymmärtää logiikkaa
+#ja toiminnallisuutta, eli ei mitään virallista tai ees mun koodia
+#Vertailin et mitä repossa ees tapahtuu!
 class SpendingsRepository:
     def __init__(self, file_path):
         self._file_path = file_path
@@ -15,6 +18,9 @@ class SpendingsRepository:
         user_spendings = filter(lambda spending: spending.user and spending.user.username == username, spendings)
 
         return list(user_spendings)
+    
+    def get_all(self):
+        pass
     
     def create(self, spending):
         spendings = self.find_all()
--- budget-app/src/repositories/user_repository.py
+++ budget-app/src/repositories/user_repository.py
@@ -1,32 +1,45 @@
 from entities.user import User
-from database_connection import get_database_connection
+#from database_connection import get_database_connection
 
-
+#Väliaikainen vaihoehto on luoda skirja, että toimii käyttäjän luonti
+#En onnistunu luoda tietokantaa vielä, joten tässä on tämän hetkinen ratkaisu
 class UserRepository:
-    def __init__(self, connection):
-        self._connection = connection
+    def __init__(self):
+        self.users = {}
     
-    def find_all(self):
-        cursor = self._connection.cursor()
-        cursor.execute("select * from users")
-        rows = cursor.fetchall()
-        return [User(row["username"], row["password"]) for row in rows]
-
-    def find_by_username(self, username):
-        cursor = self._connection.cursor()
-        cursor.execute("select * from users WHERE username = ?", (username))
-        rows = cursor.fetchone()
-        return [User(row["username"], row["password"]) for row in rows]
-
-    def create_user(self, user):
-        cursor = self._connection.cursor()
-        cursor.execute("INSERT into users (username, password) values (?, ?)", (user.username, user.password))
-        self._connection.commit()
+    def add_user(self, username, password):
+        user = User(username, password)
+        self.users[username] = user
         return user
+    
+    def find_by_username(self, username):
+        return self.users.get(username)
 
-    def delete_all(self):
-        cursor = self._connection.cursor()
-        cursor.execute("DELETE from users")
-        self._connection.commit()
+#class UserRepository:
+    #def __init__(self, connection):
+    #    self._connection = connection
+    
+    #def find_all(self):
+    #    cursor = self._connection.cursor()
+    #    cursor.execute("select * from users")
+    #    rows = cursor.fetchall()
+    #    return [User(row["username"], row["password"]) for row in rows]
 
-user_repository = UserRepository(get_database_connection())
+    #def find_by_username(self, username):
+    #    cursor = self._connection.cursor()
+    #    cursor.execute("select * from users WHERE username = ?", (username))
+    #    rows = cursor.fetchone()
+    #    return [User(row["username"], row["password"]) for row in rows]
+
+    #def create_user(self, user):
+    #    cursor = self._connection.cursor()
+    #    cursor.execute("INSERT into users (username, password) values (?, ?)", (user.username, user.password))
+    #    self._connection.commit()
+    #    return user
+
+    #def delete_all(self):
+    #    cursor = self._connection.cursor()
+    #    cursor.execute("DELETE from users")
+    #    self._connection.commit()
+
+#user_repository = UserRepository(get_database_connection())
