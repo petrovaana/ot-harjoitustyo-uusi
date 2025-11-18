17fc0f722cce033bd13bdc5fca08eedfc9fe7802
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-18 13:13:35 +0200

    fix : Modified to work with UI (might still need a little debug

--- budget-app/src/services/user_service.py
+++ budget-app/src/services/user_service.py
@@ -1,23 +1,18 @@
-from entities.user import User
-from repositories.user_repository import UserRepository
+#from entities.user import User
+from repositories.user_repository import user_repository
 
 class UserService:
-    def __init__(self):
-        self.repo = UserRepository()
-    
+    def __init__(self, repo=user_repository):
+        self.repo = repo
+
+#tää tarkistaa et onks nimi jo käytös (find_username) ja sit callaa add_userin 
     def create_user(self, username, password):
-        if self.repo.find_by_username(username):
+        if self.repo.find_username(username, password):
             return False
-        else:
-            self.repo.add_user(username, password)
-            return True
-    
-    #def user_check(self, username, password, password2):
-    #    if self.repo.find_by_username(username):
-    #        raise ValueError("Username already exists")
-    #    
-    #    if password != password2:
-    #        raise ValueError("Passwords do not match")
-    #    
-    #    if len(password) < 8:
-    #        raise ValueError("Password needs to be at least 8 characters")
+        self.repo.add_user(username, password)
+
+#Tarkistaa loginin et ois oikeet username ja salasana 
+    def login(self, username, password):
+        if not self.repo.find_account(username, password):
+            return False
+        return True
