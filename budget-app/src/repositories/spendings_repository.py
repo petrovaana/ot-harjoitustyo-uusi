694348928527948309e66eb3da1c8224d4a3d672
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-20 17:52:52 +0200

    fix: Fixed the working of some functions to desired

--- budget-app/src/repositories/spendings_repository.py
+++ budget-app/src/repositories/spendings_repository.py
@@ -1,4 +1,3 @@
-from entities.user import User
 from entities.spendings import Spendings
 from database_connection import get_database_connection
 
@@ -7,13 +6,20 @@ class SpendingsRepository:
     def __init__(self, connection):
         self._connection = connection
 
-    def find_all_spendigs_by_username(self, username):
+    def find_all_spendings_by_username(self, username):
         cursor = self._connection.cursor()
         cursor.execute(
             "select amount, content, username from logged_spendings WHERE username = ?", (username,))
         rows = cursor.fetchall()
 
-        return list(Spendings(row["amount"], row["content"], row["username"]) for row in rows)
+        return [Spendings(row[0], row[1], row[2]) for row in rows]
+
+    def user_all_logged_spendings_sum(self, username):
+        cursor = self._connection.cursor()
+        cursor.execute(
+            "select SUM(amount) from logged_spendings WHERE username = ?", (username,))
+        row = cursor.fetchone()[0]
+        return row if row else 0
 
     def add_spending(self, username, amount, content):
         cursor = self._connection.cursor()
