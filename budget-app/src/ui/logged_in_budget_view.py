dd59f48baaecd8590d883266f1b12989878fbab5
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-20 15:42:28 +0200

    fix: major bug fix, the logged in view visible now

--- budget-app/src/ui/logged_in_budget_view.py
+++ budget-app/src/ui/logged_in_budget_view.py
@@ -56,42 +56,62 @@ class LoggedInView:
         self._frame.destroy()
 
     def _logout_handler(self):
+        self.destroy()
         self.us.logout()
         self._show_login_view()
 
     def _initialize_header(self):
-        label = tk.Label(master=self._frame, text="Welcome to Budget-App!", font=("Segoe UI", 16, "bold"))
-        label.grid(row=0, column=0, columnspan=2, sticky=constants.W, padx=5, pady=5)
+        header_frame = ttk.Frame(master=self._frame)
+        header_frame.pack(fill=constants.X)
+        label = tk.Label(
+            master=header_frame,
+            text="Welcome to Budget-App!",
+            font=("Segoe UI", 16, "bold")
+        )
+
+        label.grid(row=0, column=0, padx=5, pady=5)
 
         logout_button = ttk.Button(
-            master=self._frame,
+            master=header_frame,
             text="Logout",
             command=self._logout_handler
         )
-        logout_button.grid(row=0, column=2, sticky=constants.E, padx=5, pady=5)
+        logout_button.grid(
+            row=0,
+            column=1,
+            sticky=constants.EW,
+            padx=5,
+            pady=5
+        )
 
-    def _initialize_spending_list(self, user):
+    def _initialize_spending_list(self):
         if self._spending_list_view:
             self._spending_list_view.destroy()
 
-        spendings = self.ss.get_all_spendings(user)
+        self._spending_list_frame = ttk.Frame(master=self._frame)
+        self._spending_list_frame.pack(fill=constants.BOTH, expand=True, pady=10)
 
+        spendings = self.ss.get_all_spendings(self._user)
         self._spending_list_view = SpendingsListView(
             self._spending_list_frame,
             spendings,
-            user
+            self._user
         )
-        #self._spending_list_view.grid(row=0, column=0, sticky="ew")
+
+        self._spending_list_view.pack()
 
     def _initialize_buttons(self):
+        buttons_frame = ttk.Frame(master=self._frame)
+        buttons_frame.pack(fill=constants.X, pady=10)
+
         new_spending_button = ttk.Button(
-            master=self._frame,
+            master=buttons_frame,
             text="Log in new spending",
             command=lambda: self._show_create_spending_view(self._user)
         )
 
         new_income_button = ttk.Button(
-            master=self._frame,
+            master=buttons_frame,
             text="Log in new income"
         )
 
@@ -111,9 +131,8 @@ class LoggedInView:
 
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
-        self._spending_list_frame = ttk.Frame(master=self._frame)
-        self._spending_list_frame.pack()
+        self._frame.pack(fill=constants.BOTH, expand=True)
 
         self._initialize_header()
-        self._initialize_spending_list(self._user)
+        self._initialize_spending_list()
         self._initialize_buttons()
